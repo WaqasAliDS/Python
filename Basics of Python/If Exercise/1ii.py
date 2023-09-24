340d0bb89902cf77e52259980a47a73d385b409f
WaqasAliDS <132594267+WaqasAliDS@users.noreply.github.com>
2023-09-24 10:32:59 +0500

    Create 1ii.py

---
+++ Basics of Python/If Exercise/1ii.py
@@ -0,0 +1,15 @@
+india = ["mumbai", "banglore", "chennai", "delhi"]
+pakistan = ["lahore","karachi","islamabad"]
+bangladesh = ["dhaka", "khulna", "rangpur"]
+
+city1=input("Enter the name of first city:")
+city2=input("Enter the name of second city:")
+
+if city1 in india and city2 in india:
+    print("Both cities are in India")
+elif city1 in pakistan and city2 in pakistan:
+    print("Both cities are in Pakistan")
+elif city1 in bangladesh and city2 in bangladesh:
+    print("Both cities are in Bangladesh")
+else:
+    print("They don't belong to same country")
