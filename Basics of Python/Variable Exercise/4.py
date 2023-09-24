5248ca4b0f5c925ce98d0ce5656ce84a0d61ad03
WaqasAliDS <132594267+WaqasAliDS@users.noreply.github.com>
2023-09-24 10:37:34 +0500

    Create 4.py

---
+++ Basics of Python/Variable Exercise/4.py
@@ -0,0 +1,11 @@
+#1record=2 : Invalid Variable
+#_nation=2 : Valid Variable
+#record1=2 : Valid Variable
+#record_one=2 : Valid Variable
+#record-one-=2 : Invalid Variable
+#record^one=2 : Invalid Variable
+#continue=2 : Invalid Variable
+
+
+
+
