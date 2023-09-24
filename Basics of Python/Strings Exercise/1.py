2e7dc18178aec190723f2f714f091ab777cb18ba
WaqasAliDS <132594267+WaqasAliDS@users.noreply.github.com>
2023-09-24 10:36:51 +0500

    Create 1.py

---
+++ Basics of Python/Strings Exercise/1.py
@@ -0,0 +1,13 @@
+Street="street 5"
+City="Karachi"
+Country="Pakistan"
+#1st Solution
+print('''''',Street,'''
+''',City,'''
+''',Country)
+
+#2nd Solution
+address= f'''{Street}
+{City}
+{Country}'''
+print(address)
