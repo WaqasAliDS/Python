f64f68d845e369f6e970d5b75d11beed8eb2e94c
WaqasAliDS <132594267+WaqasAliDS@users.noreply.github.com>
2023-09-24 10:24:47 +0500

    Create 1.py

---
+++ Basics of Python/For/1.py
@@ -0,0 +1,7 @@
+result = ["heads","tails","tails","heads","tails","heads","heads","tails","tails","tails"]
+Number_of_heads=0
+for i in result:
+    if i=="heads":
+        Number_of_heads=Number_of_heads+1
+    
+print(Number_of_heads)
